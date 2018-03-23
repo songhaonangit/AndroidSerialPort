991e4a803693c91efbf67c286afe3ab2fe28dbd9
anoop.hn <anoop_hn@163.com>
2018-03-23 15:14:56 +0800

    Update SerialPortActivity.java
    add switch "String" to "Hex"

--- app/src/main/java/com/kongqw/serialport/SerialPortActivity.java
+++ app/src/main/java/com/kongqw/serialport/SerialPortActivity.java
@@ -7,8 +7,10 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.kongqw.serialportlibrary.listener.OnOpenSerialPortListener;
 import com.kongqw.serialportlibrary.listener.OnSerialPortDataListener;
@@ -23,11 +25,19 @@ public class SerialPortActivity extends AppCompatActivity implements OnOpenSeria
     private static final String TAG = SerialPortActivity.class.getSimpleName();
     public static final String DEVICE = "device";
     private SerialPortManager mSerialPortManager;
-
+    ToggleButton toggleButton;
+    boolean isHex = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_serial_port);
+        toggleButton = (ToggleButton)findViewById(R.id.et_send_hex);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+    isHex = isChecked;
+            }
+        });
 
         Device device = (Device) getIntent().getSerializableExtra(DEVICE);
         Log.i(TAG, "onCreate: device = " + device);
@@ -49,7 +59,12 @@ public class SerialPortActivity extends AppCompatActivity implements OnOpenSeria
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                showToast(String.format("接收\n%s", new String(finalBytes)));
+                                if(isHex){
+                                    showToast(String.format("接收\n%s", byte2hex(finalBytes)));
+                                }else {
+                                    showToast(String.format("接收\n%s", new String(finalBytes)));
+                                }
+
                             }
                         });
                     }
@@ -62,12 +77,16 @@ public class SerialPortActivity extends AppCompatActivity implements OnOpenSeria
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                showToast(String.format("发送\n%s", new String(finalBytes)));
+                                if(isHex){
+                                    showToast(String.format("接收\n%s", byte2hex(finalBytes)));
+                                }else {
+                                    showToast(String.format("接收\n%s", new String(finalBytes)));
+                                }
                             }
                         });
                     }
                 })
-                .openSerialPort(device.getFile(), 115200);
+                .openSerialPort(device.getFile(), 9600);
 
         Log.i(TAG, "onCreate: openSerialPort = " + openSerialPort);
     }
@@ -146,11 +165,21 @@ public class SerialPortActivity extends AppCompatActivity implements OnOpenSeria
             Log.i(TAG, "onSend: 发送内容为 null");
             return;
         }
+        byte[] sendContentBytes ;
+//        byte[] sendContentBytes = {(byte) 0x01,(byte) 0x06,(byte) 0x00,(byte) 0x04,(byte) 0x00,(byte) 0x01,
+//                (byte)0x09,(byte)0xCB};
+        if(isHex){
+            sendContentBytes =String2HexBytes(sendContent);
+        }else {
+            sendContentBytes = sendContent.getBytes();
+        }
 
-        byte[] sendContentBytes = sendContent.getBytes();
 
         boolean sendBytes = mSerialPortManager.sendBytes(sendContentBytes);
         Log.i(TAG, "onSend: sendBytes = " + sendBytes);
+
+
+
         showToast(sendBytes ? "发送成功" : "发送失败");
     }
 
@@ -168,4 +197,42 @@ public class SerialPortActivity extends AppCompatActivity implements OnOpenSeria
         mToast.setText(content);
         mToast.show();
     }
+    private static String byte2hex(byte [] buffer){
+        String h = "";
+
+        for(int i = 0; i < buffer.length; i++){
+            String temp = Integer.toHexString(buffer[i] & 0xFF);
+            if(temp.length() == 1){
+                temp = "0" + temp;
+            }
+            h = h + " "+ temp;
+        }
+
+        return h;
+
+    }
+
+    public static byte[] String2HexBytes(String src) {
+        if (null == src || 0 == src.length()) {
+            return null;
+        }
+        byte[] ret = new byte[src.length() / 2];
+        byte[] tmp = src.getBytes();
+        for (int i = 0; i < (tmp.length / 2); i++) {
+            ret[i] = uniteBytes(tmp[i * 2], tmp[i * 2 + 1]);
+        }
+        return ret;
+    }
+    // byte类型数据，转成十六进制形式；
+    public static byte uniteBytes(byte src0, byte src1) {
+        byte _b0 = Byte.decode("0x" + new String(new byte[] { src0 }))
+                .byteValue();
+        _b0 = (byte) (_b0 << 4);
+        byte _b1 = Byte.decode("0x" + new String(new byte[] { src1 }))
+                .byteValue();
+        byte ret = (byte) (_b0 ^ _b1);
+        return ret;
+    }
+
+
 }
